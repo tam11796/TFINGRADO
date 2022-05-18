ee33710efe9b5dcfaf4e2beb6dfe6a6886eaf515
msn_t <msn_tamara1996@hotmail.com>
2022-05-18 20:06:07 +0200

    git remote add origin https://github.com/tam11796/TFINGRADO.git git branch -M main git push -u origin main

--- app/src/main/java/com/example/tfingrado/MainActivity.java
+++ app/src/main/java/com/example/tfingrado/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.tfingrado;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
 
+        for(int i = 0;i<menu.size();i++){
+            MenuItem menuItem = menu.getItem(i);
+            SpannableString spannable = new SpannableString(menu.getItem(i).getTitle().toString());
+            spannable.setSpan(new ForegroundColorSpan(Color.GRAY
+            ),0,spannable.length(),0);
+            menuItem.setTitle(spannable);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_save:
+                Toast.makeText(this, "Guardar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_profile:
+                Toast.makeText(this, "Nosotros", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_salir:
+                Toast.makeText(this, "Salir", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
